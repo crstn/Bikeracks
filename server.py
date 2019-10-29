9e0c2fb9f443351d793a8022b7f47edbddceed48
Carsten <kessler@plan.aau.dk>
2019-10-29 14:01:32 +0100

    Mime type error fixed

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, Response
 import psycopg2
 
 app = Flask(__name__)
@@ -10,28 +10,28 @@ def hello():
 
 @app.route('/test')
 def test():
-    lat = float(request.args.get("lat", 54))
-    lng = float(request.args.get("lng", 10))
-
-    conn = psycopg2.connect("dbname='bikeracks' \
-                         host='localhost' \
-                         user='postgres' \
-                         password='postgres'")
-
-    cur = conn.cursor()
-    cur.execute("""SELECT id, vejnavn, ST_AsGeoJSON(geom)
-                   FROM racks
-                   ORDER BY geom <-> 'SRID=4326;POINT(%s %s)'::geometry
-                   LIMIT 5;""", (lng, lat))
-    rows = cur.fetchall()
-
-    output = ""
-
-    for row in rows:
-        output = output + row[2]
-
-    cur.close()
-    conn.close()
+    # lat = float(request.args.get("lat", 54))
+    # lng = float(request.args.get("lng", 10))
+    #
+    # conn = psycopg2.connect("dbname='bikeracks' \
+    #                      host='localhost' \
+    #                      user='postgres' \
+    #                      password='postgres'")
+    #
+    # cur = conn.cursor()
+    # cur.execute("""SELECT id, vejnavn, ST_AsGeoJSON(geom)
+    #                FROM racks
+    #                ORDER BY geom <-> 'SRID=4326;POINT(%s %s)'::geometry
+    #                LIMIT 5;""", (lng, lat))
+    # rows = cur.fetchall()
+    #
+    # output = ""
+    #
+    # for row in rows:
+    #     output = output + row[2]
+    #
+    # cur.close()
+    # conn.close()
 
     output = """{
   "type": "FeatureCollection",
@@ -61,4 +61,6 @@ def test():
   ]
 }"""
 
-    return output
+    return Response(response=output,
+                    status=200,
+                    mimetype="application/json")
