a27a2bcb415dfaec2a82f221be6b06a6e4f27439
Carsten <kessler@plan.aau.dk>
2019-11-06 09:10:20 +0100

    Wikidata example added

--- static/bikeracks.js
+++ static/bikeracks.js
@@ -41,7 +41,7 @@ function onLocationError(e) {
 map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
 
-map.on('click', function(e){
+map.on('click', function(e) {
   $.ajax({
     url: "/test?lat=" + e.latlng.lat + "&lng=" + e.latlng.lng
   }).done(function(data) {
@@ -56,4 +56,47 @@ map.on('click', function(e){
 map.locate({
   setView: true,
   maxZoom: 16
+});
+
+
+
+
+function makeSPARQLQuery(endpointUrl, sparqlQuery, doneCallback) {
+  var settings = {
+    headers: {
+      Accept: 'application/sparql-results+json'
+    },
+    data: {
+      query: sparqlQuery
+    }
+  };
+  return $.ajax(endpointUrl, settings).then(doneCallback);
+}
+
+var endpointUrl = 'https://query.wikidata.org/sparql',
+  sparqlQuery = "#Map of hospitals\n" +
+  "#added 2017-08\n" +
+  "#defaultView:Map\n" +
+  "SELECT * WHERE {\n" +
+  "  ?item wdt:P31*/wdt:P279* wd:Q16917;\n" +
+  "        wdt:P625 ?geo .\n" +
+  "}LIMIT 100";
+
+makeSPARQLQuery(endpointUrl, sparqlQuery, function(data) {
+  //console.log( data );
+  data.results.bindings.forEach(function(result) {
+    cs = result.geo.value
+    url = result.item.value
+
+    coords = cs.split(" ")
+    console.log(coords)
+
+    lat = coords[1].split(")")[0]
+    lng = coords[0].split("(")[1]
+
+    L.marker([lat, lng]).on("click", function() {
+      window.open(url)
+    }).addTo(map);
+
+  })
 });
